612d03c68d95119606992aaee0024d7b02e3ba4d
Daniel Chapdelaine <orvist@gmail.com>
2024-02-22 19:51:01 -0600

    most firsts and most lasts

--- app/models/superlatives.rb
+++ app/models/superlatives.rb
@@ -2,8 +2,6 @@ require 'benchmark'
 
 class Superlatives
 
-# build every participant 
-# most times first, most times last, most selections made, least selctions
   def initialize
     @is_best = []
     @is_worst = []
@@ -36,49 +34,94 @@ class Superlatives
     {
       highest_points_scored: highest_points_scored,
       lowest_points_scored: lowest_points_scored,
-      most_firsts: [],
-      most_lasts: [],
-      most_selections: [],
-      least_selections: [],
-      most_fire_selections: [],
-      most_ice_selections: []
+      most_firsts: most_firsts,
+      most_lasts: most_lasts,
+      # most_fire_selections: [],
+      # most_ice_selections: []
     }
   end
 
+  def most_firsts
+    most_matches(@is_best)
+  end
+
+  def most_lasts
+    most_matches(@is_worst)
+  end
+
   def highest_points_scored 
-    get_top_two(@is_best)
+    top_two_scores = get_top_two_cards(@is_best)
+    dedup(top_two_scores)
   end
 
   def lowest_points_scored 
-    get_top_two(@is_worst)
+    top_two_scores = get_top_two_cards(@is_worst)
+    dedup(top_two_scores)
+  end
+
+  
+  def most_matches(list)
+    scores = {}
+    list.each do |item| 
+      if scores[item[:match]]
+        scores[item[:match]] += 1
+      else
+        scores[item[:match]] = 1
+      end
+    end
+    score_cards = scores.map {|key, value|  { name: key, score: value }}
+    sort(score_cards)
+    return get_top_two_cards(score_cards)
   end
 
   def is_best_and_is_worst
     @all_ranked_matches.each do |rankings|
       name = rankings[:name]
       rankings[:ranked_matches].each do |ranked_match| 
-        @is_best << get_score_card(name, ranked_match) if ranked_match[:is_best_match]    
-        @is_worst << get_score_card(name, ranked_match) if ranked_match[:is_worst_match]
+        @is_best << get_score_card(name, ranked_match) if ranked_match[:is_best]    
+        @is_worst << get_score_card(name, ranked_match) if ranked_match[:is_worst]
+        # @is_hot << get_score_card(name, ranked_match) if ranked_match[:is_hot]
+        # @is_warm << get_score_card(name, ranked_match) if ranked_match[:is_warm]
+        # @is_cold << get_score_card(name, ranked_match) if ranked_match[:is_cold]
       end
     end
-    @is_best.sort! { |a, b|  b[:score] <=> a[:score] }
-    @is_worst.sort! { |a, b|  b[:score] <=> a[:score] }.reverse!
+    sort(@is_best)
+    sort(@is_worst).reverse!
   end
 
-  def get_top_two(ranked)
-    top_two = []
-    ranked.each do |rank| 
-      break if top_two.map { |top| top[:score] }.uniq.count == 2
-      if top_two.first.nil? 
-        top_two << rank 
+  def dedup(list)
+    final = []
+    list.each do |rank| 
+      if final.first.nil? 
+        final << rank 
       else
         already_added = false
-        top_two.each { |top| already_added = top[:pair].sort! == rank[:pair].sort! }
-        top_two << rank if !already_added
+        final.each do |score| 
+          already_added = score[:pair].sort! == rank[:pair].sort!
+          break if already_added
+        end
+        final << rank if !already_added
+      end
+    end
+    return final
+  end 
+
+  def get_top_two_cards(ranked)
+    scores = ranked.map { |rank| rank[:score] }.uniq
+    first = scores.first
+    second = scores.second
+    top_two = []
+    ranked.each do |rank| 
+      if rank[:score] == first || rank[:score] == second
+        top_two << rank 
       end
     end
     return top_two
   end 
+
+  def sort(arr)
+    arr.sort! { |a, b|  b[:score] <=> a[:score] }
+  end
 
   def get_score_card(name, match)
     {
